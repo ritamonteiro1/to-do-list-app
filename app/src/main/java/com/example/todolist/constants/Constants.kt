5951e4aea7b960248025b5adc352212cd9b8a6eb
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-26 10:55:45 -0300

    create DateExtensions.kt

--- app/src/main/java/com/example/todolist/constants/Constants.kt
+++ app/src/main/java/com/example/todolist/constants/Constants.kt
@@ -1,7 +1,10 @@
 package com.example.todolist.constants
 
+import java.util.*
+
 class Constants {
-    companion object{
+    companion object {
         const val DATE_PICKER = "date picker"
+        const val DATE_FORMAT = "dd/MM_yyyy"
     }
 }
--- app/src/main/java/com/example/todolist/extensions/DateExtensions.kt
+++ app/src/main/java/com/example/todolist/extensions/DateExtensions.kt
@@ -1,2 +1,11 @@
 package com.example.todolist.extensions
 
+import com.example.todolist.constants.Constants
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+fun Date.format(): String {
+    val localeBr = Locale("pt", "BR")
+    return SimpleDateFormat(Constants.DATE_FORMAT, localeBr).format(this)
+}
