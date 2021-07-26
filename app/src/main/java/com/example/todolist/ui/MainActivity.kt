b0ca9644a8c64006fb78cf84a870bc76feb3db60
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-26 13:43:10 -0300

    create item_task.xml

--- app/src/main/java/com/example/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/example/todolist/ui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.todolist.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.todolist.R
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.mainFloatingActionButton.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
